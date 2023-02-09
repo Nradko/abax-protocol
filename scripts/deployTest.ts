e722ef5576b336f03f0ad3200b06e4fe6c62c5fd
WookashWackomy <wookie.xp.07@gmail.com>
2023-02-09 18:23:17 +0100

    enable deploy test without mocked time

--- scripts/deployTest.ts
+++ scripts/deployTest.ts
@@ -4,14 +4,17 @@ import { argvObj } from './compile/common';
 import chalk from 'chalk';
 import { apiProviderWrapper } from 'tests/setup/helpers';
 
-(async (args: Record<string, unknown>) => {
+(async (args: Record<string, string>) => {
   if (require.main !== module) return;
-  const outputJsonFolder = (args['path'] as string) ?? process.argv[2] ?? process.env.PWD;
+  const outputJsonFolder = args['path'] ?? process.env.PWD;
+  const shouldUseMockTimestamp = (args['shouldUseMockTimestamp'] === 'true' || args['shouldUseMockTimestamp'] === '1') ?? true;
+  console.log(shouldUseMockTimestamp);
+  console.log(args);
   if (!outputJsonFolder) throw 'could not determine path';
   const api = await apiProviderWrapper.getAndWaitForReady();
   const deployPath = path.join(outputJsonFolder, 'deployedContracts.json');
 
-  await deployAndConfigureSystem(undefined, deployPath);
+  await deployAndConfigureSystem({ shouldUseMockTimestamp }, deployPath);
   api.disconnect();
   process.exit(0);
 })(argvObj).catch((e) => {
