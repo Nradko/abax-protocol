bed81478ff61d4f353ebeca480220a607cb55a69
DesiredDesire <konrad.wierzbik@gmail.com>
2023-06-28 17:59:10 +0200

    roles

--- tests/consts.ts
+++ tests/consts.ts
@@ -1,11 +1,5 @@
 import BN from 'bn.js';
 
-//TODO populate/remove after addition of roles tests
-export const Roles = {
-  DefaultAdminRole: 0,
-  Minter: 0xfd9ab216,
-};
-
 export enum RateMode {
   None = '0',
   Stable = '1',
@@ -19,10 +13,12 @@ export enum LendingToken {
 }
 export const ONE_YEAR = new BN(365 * 24 * 60 * 60 * 1000);
 
+export const MINTER = 4254773782;
+
+export const ROLE_ADMIN = 0;
 export const FLASH_BORROWER = 1112475474;
 export const ASSET_LISTING_ADMIN = 1094072439;
 export const PARAMETERS_ADMIN = 368001360;
 export const EMERGENCY_ADMIN = 297099943;
 export const GLOBAL_ADMIN = 2459877095;
-export const ROLE_ADMIN = 0;
 export const TREASURY = 2434241257;
