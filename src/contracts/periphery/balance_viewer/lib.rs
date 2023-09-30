2facaf48381169af2988a9e512f5764e9e615d01
DesiredDesire <konrad.wierzbik@gmail.com>
2023-09-30 17:06:47 +0200

    added view CompleteReserveData to a periphery contract

--- src/contracts/periphery/balance_viewer/lib.rs
+++ src/contracts/periphery/balance_viewer/lib.rs
@@ -3,8 +3,14 @@
 #[ink::contract]
 pub mod balance_viewer {
 
+    use lending_project::impls::lending_pool::storage::structs::reserve_data;
+    use lending_project::impls::lending_pool::storage::structs::reserve_data::ReserveAbacusTokens;
     use lending_project::impls::lending_pool::storage::structs::reserve_data::ReserveData;
+    use lending_project::impls::lending_pool::storage::structs::reserve_data::ReserveParameters;
+    use lending_project::impls::lending_pool::storage::structs::reserve_data::ReservePrice;
+    use lending_project::impls::lending_pool::storage::structs::reserve_data::ReserveRestrictions;
     use lending_project::impls::lending_pool::storage::structs::user_reserve_data::UserReserveData;
+    use lending_project::traits::lending_pool::errors::LendingPoolError;
     // use lending_project::traits::managing::*;
     use pendzl::{contracts::psp22::*, traits::Storage};
 
@@ -12,18 +18,20 @@ pub mod balance_viewer {
     use lending_project::traits::lending_pool::traits::view::LendingPoolView;
     use lending_project::traits::lending_pool::traits::view::LendingPoolViewRef;
 
+    #[derive(Debug, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct CompleteReserveData {
+        pub data: Option<ReserveData>,
+        pub parameters: Option<ReserveParameters>,
+        pub restriction: Option<ReserveRestrictions>,
+        pub price: Option<ReservePrice>,
+        pub tokens: Option<ReserveAbacusTokens>,
+    }
+
     #[ink(storage)]
     #[derive(Storage)]
     pub struct BalanceViewer {
-        lending_pool: AccountId,
-    }
-
-    impl Default for BalanceViewer {
-        fn default() -> Self {
-            Self {
-                lending_pool: ink::blake2x256!("ZERO_ADRESS").into(),
-            }
-        }
+        lending_pool: LendingPoolViewRef,
     }
 
     // impl Managing for PSP22EmitableContract {}
@@ -31,9 +39,9 @@ pub mod balance_viewer {
     impl BalanceViewer {
         #[ink(constructor)]
         pub fn new(lending_pool: AccountId) -> Self {
-            let mut instance = Self::default(); // metadata
-            instance.lending_pool = lending_pool;
-            instance
+            BalanceViewer {
+                lending_pool: lending_pool.into(),
+            }
         }
 
         #[ink(message)]
@@ -42,10 +50,8 @@ pub mod balance_viewer {
             assets: Option<Vec<AccountId>>,
             user: AccountId,
         ) -> Vec<(AccountId, Balance)> {
-            let assets_to_view = assets.unwrap_or_else(|| {
-                let lending_pool: LendingPoolViewRef = self.lending_pool.into();
-                lending_pool.view_registered_assets()
-            });
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
 
             let mut ret: Vec<(AccountId, Balance)> = vec![];
             for asset in assets_to_view {
@@ -60,15 +66,14 @@ pub mod balance_viewer {
             &self,
             assets: Option<Vec<AccountId>>,
         ) -> Vec<(AccountId, Option<ReserveData>)> {
-            let lending_pool: LendingPoolViewRef = self.lending_pool.into();
-            let assets_to_view =
-                assets.unwrap_or_else(|| lending_pool.view_registered_assets());
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
 
             let mut ret: Vec<(AccountId, Option<ReserveData>)> = vec![];
             for asset in assets_to_view {
                 ret.push((
                     asset,
-                    lending_pool.view_unupdated_reserve_data(asset),
+                    self.lending_pool.view_unupdated_reserve_data(asset),
                 ));
             }
             ret
@@ -78,13 +83,12 @@ pub mod balance_viewer {
             &self,
             assets: Option<Vec<AccountId>>,
         ) -> Vec<(AccountId, Option<ReserveData>)> {
-            let lending_pool: LendingPoolViewRef = self.lending_pool.into();
-            let assets_to_view =
-                assets.unwrap_or_else(|| lending_pool.view_registered_assets());
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
 
             let mut ret: Vec<(AccountId, Option<ReserveData>)> = vec![];
             for asset in assets_to_view {
-                ret.push((asset, lending_pool.view_reserve_data(asset)));
+                ret.push((asset, self.lending_pool.view_reserve_data(asset)));
             }
             ret
         }
@@ -95,15 +99,14 @@ pub mod balance_viewer {
             assets: Option<Vec<AccountId>>,
             account: AccountId,
         ) -> Vec<(AccountId, UserReserveData)> {
-            let lending_pool: LendingPoolViewRef = self.lending_pool.into();
-            let assets_to_view =
-                assets.unwrap_or_else(|| lending_pool.view_registered_assets());
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
 
             let mut ret: Vec<(AccountId, UserReserveData)> = vec![];
             for asset in assets_to_view {
                 ret.push((
                     asset,
-                    lending_pool
+                    self.lending_pool
                         .view_unupdated_user_reserve_data(asset, account),
                 ));
             }
@@ -115,16 +118,44 @@ pub mod balance_viewer {
             assets: Option<Vec<AccountId>>,
             account: AccountId,
         ) -> Vec<(AccountId, UserReserveData)> {
-            let lending_pool: LendingPoolViewRef = self.lending_pool.into();
-            let assets_to_view =
-                assets.unwrap_or_else(|| lending_pool.view_registered_assets());
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
 
             let mut ret: Vec<(AccountId, UserReserveData)> = vec![];
             for asset in assets_to_view {
                 ret.push((
                     asset,
-                    lending_pool.view_user_reserve_data(asset, account),
+                    self.lending_pool.view_user_reserve_data(asset, account),
                 ));
+            }
+            ret
+        }
+
+        #[ink(message)]
+        pub fn view_complete_reserve_data(
+            &self,
+            asset: AccountId,
+        ) -> CompleteReserveData {
+            CompleteReserveData {
+                data: self.lending_pool.view_reserve_data(asset),
+                parameters: self.lending_pool.view_reserve_parameters(asset),
+                restriction: self.lending_pool.view_reserve_restrictions(asset),
+                price: self.lending_pool.view_reserve_prices(asset),
+                tokens: self.lending_pool.view_reserve_tokens(asset),
+            }
+        }
+
+        #[ink(message)]
+        pub fn view_complete_reserve_datas(
+            &self,
+            assets: Option<Vec<AccountId>>,
+        ) -> Vec<(AccountId, CompleteReserveData)> {
+            let assets_to_view = assets
+                .unwrap_or_else(|| self.lending_pool.view_registered_assets());
+
+            let mut ret: Vec<(AccountId, CompleteReserveData)> = vec![];
+            for asset in assets_to_view {
+                ret.push((asset, self.view_complete_reserve_data(asset)));
             }
             ret
         }
