bc31d95d8425374a06ad6350e80ae7af69275219
DesiredDesire <konrad.wierzbik@gmail.com>
2023-10-21 11:38:38 +0200

    update cargos, add price feeder

---
+++ src/contracts/core/price_feed_provider/lib.rs
@@ -0,0 +1,84 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[pendzl::implementation(Ownable)]
+#[ink::contract]
+pub mod price_feed_provider {
+    use ink::prelude::string::String;
+    use ink::storage::{Lazy, Mapping};
+
+    use lending_project::traits::dia_oracle::getter::{OracleGettersRef, *};
+    use lending_project::traits::price_feed::price_feed::{
+        PriceFeed, PriceFeedError,
+    };
+    use pendzl::contracts::ownable::{OwnableError, OwnableImpl};
+    use pendzl::traits::Storage;
+
+    #[ink(storage)]
+    #[derive(Storage, Default)]
+    pub struct PriceFeedProvider {
+        #[storage_field]
+        ownable: ownable::Data,
+        oracle: Lazy<OracleGettersRef>,
+        account_to_symbol: Mapping<AccountId, String>,
+    }
+
+    impl PriceFeedProvider {
+        #[ink(constructor)]
+        pub fn new(oracle: AccountId) -> Self {
+            let mut instance: PriceFeedProvider = Default::default();
+            ownable::Internal::_init_with_owner(
+                &mut instance,
+                Self::env().caller(),
+            );
+            let oracle_ref: OracleGettersRef = oracle.into();
+            instance.oracle.set(&oracle_ref);
+            instance
+        }
+
+        #[ink(message)]
+        pub fn set_account_symbol(
+            &mut self,
+            asset: AccountId,
+            symbol: String,
+        ) -> Result<(), OwnableError> {
+            self._only_owner()?;
+            self.account_to_symbol.insert(&asset, &symbol);
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn get_account_symbol(
+            &mut self,
+            asset: AccountId,
+        ) -> Option<String> {
+            self.account_to_symbol.get(&asset)
+        }
+    }
+
+    impl PriceFeed for PriceFeedProvider {
+        #[ink(message)]
+        fn get_latest_prices(
+            &self,
+            assets: Vec<AccountId>,
+        ) -> Result<Vec<u128>, PriceFeedError> {
+            let mut result: Vec<u128> = vec![];
+            let mut symbols: Vec<String> = vec![];
+
+            for asset in assets {
+                let symbol = self
+                    .account_to_symbol
+                    .get(&asset)
+                    .ok_or(PriceFeedError::NoSuchAsset)?;
+                symbols.push(symbol)
+            }
+            let oracle: OracleGettersRef = self.oracle.get().unwrap();
+            for res in oracle.get_latest_prices(symbols) {
+                match res {
+                    Some(r) => result.push(r.1),
+                    None => return Err(PriceFeedError::NoPriceFeed),
+                }
+            }
+            Ok(result)
+        }
+    }
+}
--- src/impls/constants.rs
+++ src/impls/constants.rs
@@ -1,6 +1,8 @@
 pub const E6_U128: u128 = 10_u128.pow(6);
 pub const E8_U128: u128 = 10_u128.pow(8);
 pub const E9_U128: u128 = 10_u128.pow(9);
+
+pub const E10_U128: u128 = 10_u128.pow(10);
 pub const E12_U128: u128 = 10_u128.pow(12);
 pub const E15_U128: u128 = 10_u128.pow(15);
 pub const E18_U128: u128 = 10_u128.pow(18);
