4987dc6d61f700856612377f837f20f65ccbb027
DesiredDesire <konrad.wierzbik@gmail.com>
2023-11-03 17:00:39 +0100

    add stable_token contract

---
+++ src/contracts/core/stable_token/lib.rs
@@ -0,0 +1,116 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[pendzl::implementation(
+    PSP22,
+    PSP22Metadata,
+    AccessControl,
+    PSP22Mintable,
+    PSP22Burnable
+)]
+#[ink::contract]
+pub mod stable_token {
+    use ink::{
+        codegen::{EmitEvent, Env},
+        prelude::string::String,
+    };
+
+    use lending_project::impls::constants::{BURNER, MINTER};
+    use pendzl::{
+        contracts::{
+            access_control,
+            psp22::{
+                extensions::{metadata::*, mintable::PSP22MintableImpl},
+                PSP22Error,
+            },
+        },
+        traits::Storage,
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct StableToken {
+        #[storage_field]
+        access: access_control::Data,
+        #[storage_field]
+        psp22: psp22::Data,
+        #[storage_field]
+        metadata: metadata::Data,
+    }
+
+    impl StableToken {
+        #[ink(constructor)]
+        pub fn new(name: String, symbol: String, decimal: u8) -> Self {
+            let mut instance = Self::default();
+            instance.metadata.name.set(&name.into());
+            instance.metadata.symbol.set(&symbol.into());
+            instance.metadata.decimals.set(&decimal);
+            instance._init_with_caller();
+            instance
+        }
+    }
+
+    #[overrider(PSP22Mintable)]
+    pub fn mint(
+        &mut self,
+        account: AccountId,
+        amount: Balance,
+    ) -> Result<(), PSP22Error> {
+        self._ensure_has_role(MINTER, Some(self.env().caller()))?;
+        self._mint_to(account, amount)
+    }
+
+    #[overrider(PSP22Burnable)]
+    pub fn burn(
+        &mut self,
+        account: AccountId,
+        amount: Balance,
+    ) -> Result<(), PSP22Error> {
+        self._ensure_has_role(BURNER, Some(self.env().caller()))?;
+        self._burn_from(account, amount)
+    }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
+
+    #[overrider(psp22::Internal)]
+    fn _emit_transfer_event(
+        &self,
+        from: Option<AccountId>,
+        to: Option<AccountId>,
+        amount: Balance,
+    ) {
+        self.env().emit_event(Transfer {
+            from,
+            to,
+            value: amount,
+        })
+    }
+    #[overrider(psp22::Internal)]
+    fn _emit_approval_event(
+        &self,
+        owner: AccountId,
+        spender: AccountId,
+        amount: Balance,
+    ) {
+        self.env().emit_event(Approval {
+            owner,
+            spender,
+            value: amount,
+        })
+    }
+}
--- src/impls/constants.rs
+++ src/impls/constants.rs
@@ -1,3 +1,5 @@
+use pendzl::contracts::access_control::RoleType;
+
 pub const E6_U128: u128 = 10_u128.pow(6);
 pub const E8_U128: u128 = 10_u128.pow(8);
 pub const E9_U128: u128 = 10_u128.pow(9);
@@ -11,3 +13,35 @@ pub const ONE_HOUR: u128 = 60 * 60 * 1000;
 pub const DAY: u128 = 24 * ONE_HOUR;
 pub const MATH_ERROR_MESSAGE: &'static str =
     "An unexpected math error occured!";
+
+// can mint given asset
+pub const MINTER: RoleType = ink::selector_id!("MINTER");
+
+// can burn given asset
+pub const BURNER: RoleType = ink::selector_id!("BURNER");
+
+/// pays only 10% of standard flash loan fee
+pub const FLASH_BORROWER: RoleType = ink::selector_id!("FLASH_BORROWER"); // 1_112_475_474_u32
+
+/// can add new asset to the market
+pub const ASSET_LISTING_ADMIN: RoleType =
+    ink::selector_id!("ASSET_LISTING_ADMIN"); // 1_094_072_439_u32
+
+/// can modify reserveData parameters
+pub const PARAMETERS_ADMIN: RoleType = ink::selector_id!("PARAMETERS_ADMIN"); // 368_001_360_u32
+
+/// can modify current debt rate in ReserveData of asset that is protocol stablecoin
+pub const STABLECOIN_RATE_ADMIN: RoleType =
+    ink::selector_id!("STABLECOIN_RATE_ADMIN"); // 2_742_621_032
+
+/// can pause/unpause freeze/unfreeze reserves
+pub const EMERGENCY_ADMIN: RoleType = ink::selector_id!("EMERGENCY_ADMIN"); // 297_099_943_u32
+
+/// can do what ASSET_LISTING_ADMIN, PARAMETERS_ADMIN and EMERGANCY_ADMIN can do
+pub const GLOBAL_ADMIN: RoleType = ink::selector_id!("GLOBAL_ADMIN"); // 2_459_877_095_u32
+
+/// can assign all the roles
+pub const ROLE_ADMIN: RoleType = 0; // 0
+
+/// can withdraw protocol income
+pub const TREASURY: RoleType = ink::selector_id!("TREASURY"); // 2_434_241_257_u32
