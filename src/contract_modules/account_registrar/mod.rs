bb10044f27d898db95abe36ccb7a8a1f61b8b8a7
DesiredDesire <konrad.wierzbik@gmail.com>
2024-03-27 13:24:10 +0100

    move account_registrar module to seprerate folder

---
+++ src/contract_modules/account_registrar/mod.rs
@@ -0,0 +1,5 @@
+pub mod implementation;
+
+mod tratis;
+
+pub use tratis::*;
--- src/contract_modules/lending_pool/traits/mod.rs
+++ src/contract_modules/lending_pool/traits/mod.rs
@@ -1,5 +1,4 @@
 mod a_token_interface;
-mod account_registrar;
 mod lending_pool_actions;
 mod lending_pool_flash;
 mod lending_pool_maintain;
@@ -8,7 +7,6 @@ mod lending_pool_view;
 mod v_token_interface;
 
 pub use a_token_interface::*;
-pub use account_registrar::*;
 pub use lending_pool_actions::*;
 pub use lending_pool_flash::*;
 pub use lending_pool_maintain::*;
